8c0ecff6dcfc6c50ff81457af64f663e3ea94726
AbhinavDwarapu <abhinavdwarapu+github@gmail.com>
2026-09-10 23:57:29 +0100

    update title component

--- app/components/title.tsx
+++ app/components/title.tsx
@@ -6,7 +6,7 @@ export function TitleCard() {
     const formattedDate = `${month} ${day}, ${year}`;
     return (
         <div className="flex flex-row items-baseline gap-4">
-            <div className="text-3xl font-alfa-slab-one">Connections</div>
+            <div className="text-3xl font-alfa-slab-one">(Dis)Connections</div>
             <div className="text-[28px] font-news-cycle">{formattedDate}</div>
         </div>
     )
